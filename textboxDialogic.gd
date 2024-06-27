1ed4a1c0d622954c709789075b2ce49271333f67
IgnacioCordiglia <ignaciocordiglia.contactos@gmail.com>
2024-06-27 17:50:35 -0300

    new dialogue implemented

---
+++ textboxDialogic.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.start("pcGuy")
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
